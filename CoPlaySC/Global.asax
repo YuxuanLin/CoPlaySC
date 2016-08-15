9a9bbdafac5b906ecea3bc540d657fe565b8cd65
linyuxuan <linyuxuan@LINYUXUANE6C0>
2016-08-15 16:30:58 +1000

    Add project files.

---
+++ CoPlaySC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CoPlaySC.MvcApplication" Language="C#" %>
---
+++ CoPlaySC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CoPlaySC.Startup))]
+namespace CoPlaySC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
